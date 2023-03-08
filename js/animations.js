9a1e23981577545d5435b157afe013baa3bc145e
ValentinValdeon <valentinvaldeon@gmail.com>
2023-03-08 00:59:52 -0300

    Agregando JS y arreglando estructura

---
+++ js/animations.js
@@ -0,0 +1,71 @@
+
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk1');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk2');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk3');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk4');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk5');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk6');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+    
+    window.addEventListener('scroll', function(){
+        let animacion1 = document.getElementById('sk7');
+        let posicionSk = animacion1.getBoundingClientRect().top-10;
+        let tamanioPantalla = window.innerHeight;
+    
+        if(posicionSk < tamanioPantalla){
+            animacion1.style.animation = 'mover 1.4s ease-out';
+        }
+    })
+
